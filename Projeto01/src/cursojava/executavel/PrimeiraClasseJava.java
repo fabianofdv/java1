825968e1b6df049590ebb0b439d06d3a1fb7a88a
fleic <fleic@192.168.1.41>
2022-04-10 08:08:40 -0300

    Criação do projeto
    comitando projeto

---
+++ Projeto01/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,14 @@
+package cursojava.executavel;
+
+
+public class PrimeiraClasseJava {
+	
+	/*Main eh o método auto executável em Java*/ 
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em java - do curso Formação Javaweb");
+		System.out.println("Java é orientado a objetos.");
+	}
+	
+}
