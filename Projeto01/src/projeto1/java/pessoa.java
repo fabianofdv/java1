5ea67b6e56a5172d37692f10b2ff98a990df911c
fabiano araujo <f.nsilva25@gmail.com>
2022-04-10 08:51:21 -0300

    Nova alteração

--- Projeto01/src/projeto1/java/pessoa.java
+++ Projeto01/src/projeto1/java/pessoa.java
@@ -1,5 +1,27 @@
 package projeto1.java;
 
 public class pessoa {
+	
+	private String nome;
+	
+	private String sobrenome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
 
 }
